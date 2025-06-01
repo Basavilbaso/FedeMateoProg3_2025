5bf1705005fd16c9becb501c812f7fe2bdb2d265
Basavilbaso <fbasavilbaso@udesa.edu.ar>
2025-06-01 00:46:19 -0300

    Configuracion de firebase y Menu de navegacion (punto 1)

---
+++ proyectoProg3-rn/src/navigation/BottonTabs.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { FontAwesome } from '@expo/vector-icons'
+import { AntDesign } from '@expo/vector-icons';
+
+import Home from '../screens/Home';
+import Profile from '../screens/Profile';
+import NewPost from '../screens/NewPost';
+
+const Tab= createBottomTabNavigator();
+
+function BottomTabs() {
+  return (
+    <Tab.Navigator>
+        <Tab.Screen name="Home" component={Home} options={{headerShown: false, tabBarIcon: () => <FontAwesome name="home" size={24} color="black" />}}  />
+        <Tab.Screen name="Profile" component={Profile} options={{headerShown: false, tabBarIcon: () => <AntDesign name="bars" size={24} color="black" />}}  />
+        <Tab.Screen name="Post" component={NewPost} options={{headerShown: false, tabBarIcon: () => <AntDesign name="bars" size={24} color="black" />}}  />
+    </Tab.Navigator>
+  )
+}
+export default BottomTabs;
---
+++ proyectoProg3-rn/src/navigation/StackNavigation.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import BottomTabs from "./BottonTabs";
+import Login from "../screens/Login";
+import Register from "../screens/Register";
+
+const Stack = createNativeStackNavigator();
+
+function StackNavigation(){
+    return(
+        <Stack.Navigator>
+            <Stack.Screen
+                name='Login'
+                component={Login}
+                options={{ headerShown: false }} />
+            <Stack.Screen
+                name='Register'
+                component={Register}
+                options={{ headerShown: false }} />
+            <Stack.Screen
+                name='Home'
+                component={BottomTabs}
+                options={{ headerShown: false }} />
+        </Stack.Navigator>
+    )
+}
+export default StackNavigation;

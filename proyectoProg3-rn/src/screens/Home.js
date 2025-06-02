2318e23d7af0a5d0982c331e338a8b4c4d1b81b4
Basavilbaso <fbasavilbaso@udesa.edu.ar>
2025-06-01 23:18:30 -0300

    Home base

---
+++ proyectoProg3-rn/src/screens/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { Text } from "react-native"
+
+class Home extends Component() {
+
+    render() {
+        return (
+            <View>
+                <Text></Text>
+            </View>
+        )
+    }
+}
+export default Home;
